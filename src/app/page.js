9e18e2d99fb5429c38c5787f165f62de4d9afe12
Octagon <ugorji757@gmail.com>
2023-10-30 01:02:02 +0100

    completed hero and experience section

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
       <section className="hero is-medium">
         <div className="hero-body is-justify-content-center">
           <div className='container'>
-            <div className='columns'>
-              <div className='column is-align-self-center'>
+            <div className={`columns ${styles.heroColumns}`}>
+              <div className={`column is-align-self-center ${styles.heroColumnsItem}`}>
                 <div className={styles.greetingsContainer}>
                   <p>👋 Greetings!</p>
                 </div>
@@ -25,25 +25,70 @@ export default function Home() {
                   </a>
                 </div>
               </div>
-              <div className='column'>
+              <div className={`column is-align-self-center ${styles.heroColumnsItem}`}>
                 <Image className={styles.heroMyMe} src={'./my-me.svg'} width={500} height={500} alt='Simon Ugorji' />
               </div>
-              <div className='column is-align-self-center has-text-right'>
+              <div className={`column is-align-self-center ${styles.heroColumnsItem}`}>
                 <div className={styles.heroContactButtons}>
                   <a className={styles.hasIcon} href=''>
                     Download CV <span className='icon'>
                       <Image src="./arrowLineDown.svg" alt='Download CV Icon' width={20} height={20} /></span>
                   </a>
-                  <button className={`button is-brand-outline ${styles.hasIcon}`}>
+                  <button className={`button ${styles.isHeroOutline} ${styles.hasIcon}`}>
                     <span className='icon'>
-                    <Image src="./WhatsappLogo.svg" alt="Chat me up on whatsapp" width={20} height={20} />
+                      <Image src="./WhatsappLogo.svg" alt="Chat me up on whatsapp" width={20} height={20} />
                     </span>Ping me on whatsapp
-                    </button>
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </div >
+      </section>
+      <section className={styles.sectionExperience}>
+        <div className={styles.experienceCards}>
+          <div className={styles.experienceItem}>
+            <div className={styles.experienceIcon}>
+              <Image src="./CodeIcon.svg" alt="De" width={80} height={80} />
+            </div>
+            <div className={styles.experienceContent}>
+              <p>3 years as</p>
+              <p>Programmer</p>
+            </div>
+          </div>
+          <div className={styles.experienceItem}>
+            <div className={styles.experienceIcon}>
+              <Image src="./ProjectsIcon.svg" alt="De" width={80} height={80} />
+            </div>
+            <div className={styles.experienceContent}>
+              <p>7 years of</p>
+              <p>Work</p>
+            </div>
+          </div>
+          <div className={styles.experienceItem}>
+            <div className={styles.experienceIcon}>
+              <Image src="./DesignIcon.svg" alt="De" width={80} height={80} />
+            </div>
+            <div className={styles.experienceContent}>
+              <p>4 years of</p>
+              <p>Technical Writing</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.experienceDescription}>
+          <div className={styles.descriptionItem}>
+            <p>Fullstack</p>
+            <p>Web Developer</p>
+          </div>
+          <div className={styles.descriptionItem}>
+            <p>Dozens of projects and</p>
+            <p>Experiences</p>
+          </div>
+          <div className={styles.descriptionItem}>
+            <p>Proficient</p>
+            <p>Technical Writer</p>
+          </div>
+        </div>
       </section>
     </main>
   )
