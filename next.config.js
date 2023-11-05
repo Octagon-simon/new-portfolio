9030312c392155e042619525616f901c5f085087
Octagon <ugorji757@gmail.com>
2023-11-05 15:47:14 +0100

    modified next.config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+// next.config.js
+const nextConfig = {
+    output: 'export',
+    images : {
+        unoptimized : true
+    },
+    basePath : ""
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: '/home',
+    //             destination: '/',
+    //             permanent: true,
+    //         },
+    //     ];
+    // },
+}
 
 module.exports = nextConfig
